837fc148174f7183b12e29156773e34000adaffe
Thlast <santiagoturini@gmail.com>
2023-05-01 15:39:15 -0300

    context para configuraciones del usuario

---
+++ src/context/UserConfig.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+
+export const UserConfig = React.createContext({})
+
+export function UserConfigProvider({ children }) {
+
+  const [mobile, setMobile] = useState(window.innerWidth <= 500)
+
+  //funcion para detectar si es mobile
+  useEffect(() => {
+    function handleResize() {
+      setMobile(window.innerWidth <= 770);
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  //cambio de tema
+  const selectedTheme = localStorage.getItem('selected-theme')
+
+  useEffect(() => {
+    checkTema()
+  }, [])
+
+  const getCurrentTheme = () => document.body.classList.contains('dark-theme') ? 'dark' : 'light'
+
+  const checkTema = () => {
+    const userHasDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+    if (selectedTheme) {
+      // Si se cumple la validación, preguntamos cuál fue el tema para saber si activamos o desactivamos el dark
+      document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove']('dark-theme')
+    } else {
+      // Preguntamos si el usuario tiene tema dark en su sistema
+      // En caso de que sí, lo activamos en la interfaz
+      if (userHasDarkTheme) document.body.classList.add('dark-theme')
+    }
+
+  }
+  const switchTema = () => {
+    document.body.classList.toggle('dark-theme')
+    // Guardamos el tema actual que eligió el usuario
+    localStorage.setItem('selected-theme', getCurrentTheme())
+  }
+
+  return (
+    <UserConfig.Provider value={{
+      mobile,
+      switchTema
+    }}>
+      {children}
+    </UserConfig.Provider>
+  )
+}
