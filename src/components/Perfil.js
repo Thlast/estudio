909ef46f5578c666fd6c81a9646fb3cbdd5db548
Thlast <santiagoturini@gmail.com>
2023-05-01 15:38:17 -0300

    funciones del nav pasan al menu en mobile

--- src/components/Perfil.js
+++ src/components/Perfil.js
@@ -8,10 +8,12 @@ import { db } from '../firebase';
 import { doc, setDoc } from 'firebase/firestore';
 import { getAuth, updateProfile } from "firebase/auth";
 import style from '../modulos-css/Perfil.module.css'
+import { UserConfig } from '../context/UserConfig';
 export function Perfil() {
 
+  const { mobile, switchTema } = useContext(UserConfig);
   const { reiniciarHistorial } = useContext(ResueltasContext)
-  const { loading, datosUser, user, obtenerUser } = useAuth();
+  const { loading, datosUser, user, obtenerUser, editMode, changeEditMode } = useAuth();
   const auth = getAuth();
   const navigate = useNavigate();
   const [editUser, setEditUser] = useState(false)
@@ -19,8 +21,8 @@ export function Perfil() {
 
   const modNombre = async (nombreUser, e) => {
     e.preventDefault()
-    if (nombreUser.length > 0){
-      await setDoc(doc(db, "usuarios/"+user.uid), { nombre: nombreUser}, {merge: true});
+    if (nombreUser.length > 0) {
+      await setDoc(doc(db, "usuarios/" + user.uid), { nombre: nombreUser }, { merge: true });
       updateProfile(auth.currentUser, {
         displayName: nombreUser
       }).then(() => {
@@ -31,10 +33,10 @@ export function Perfil() {
       }).catch((error) => {
         alert("An error occurred " + error)
 
-      }); 
+      });
     }
-    
-      else {
+
+    else {
       alert("Escribe un nombre")
     }
   }
@@ -44,7 +46,7 @@ export function Perfil() {
     alertalimpiarHistorialUsuario(reiniciarHistorial)
   }
 
-  
+
 
   if (loading) return <h1>Loading...</h1>
 
@@ -55,43 +57,62 @@ export function Perfil() {
           <nav >
             <ul>
               <li>
-              {editUser ?
-              <form>
-                <input
-                  className={style.usermodificar}
-                  placeholder={datosUser?.nombre}
-                  onChange={(e) => setNombreUser(e.target.value)}
-                >
-                </input>
-                <span>
-                  <button
-                    onClick={(e) => modNombre(nombreUser, e)}
-                    className={style.botonconfirmar}
+                {editUser ?
+                  <form>
+                    <input
+                      className={style.usermodificar}
+                      placeholder={datosUser?.nombre}
+                      onChange={(e) => setNombreUser(e.target.value)}
                     >
-                    ✓
-                  </button>
-                  <button
-                  type='button'
-                    onClick={() => setEditUser(false)}
-                    className={style.botoncancelar}
+                    </input>
+                    <span>
+                      <button
+                        onClick={(e) => modNombre(nombreUser, e)}
+                        className={style.botonconfirmar}
+                      >
+                        ✓
+                      </button>
+                      <button
+                        type='button'
+                        onClick={() => setEditUser(false)}
+                        className={style.botoncancelar}
+                      >
+                        ✘
+                      </button>
+                    </span>
+                  </form>
+                  : <div>
+                    <span>
+                      {datosUser?.rol}: {datosUser?.nombre}
+                    </span>
+                    <button
+                      onClick={() => setEditUser(true)}
+                      className={style.botoneditar}
                     >
-                    ✘
-                  </button>
-                </span>
-              </form>
-              : <div>
-                <span>
-                  {datosUser?.nombre}
-                </span>
-                <button
-                  onClick={() => setEditUser(true)}
-                  className={style.botoneditar}
-                  >
-                  &#9999;
-                </button>
-              </div>}
+                      &#9999;
+                    </button>
+                  </div>}
               </li>
               <hr></hr>
+              {mobile &&
+                <>
+
+                  <button
+                    className={editMode ? style.botonActivo : style.links}
+                    onClick={() => changeEditMode(datosUser?.rol)}
+                  >
+                    Editar:{editMode ? " on" : " off"}
+                  </button>
+
+                  <button
+                    className="switchTema"
+                    onClick={() => switchTema()}>
+                  </button>
+
+                  <hr></hr>
+
+                </>
+              }
               <li>
                 <SelectMateria />
               </li>
--- src/components/navbarr.js
+++ src/components/navbarr.js
@@ -1,41 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { getAuth } from "firebase/auth";
 import { signOut } from "firebase/auth";
 import style from '../modulos-css/navbarr.module.scss'
+import { UserConfig } from '../context/UserConfig';
 
 export function Nav() {
 
-  //cambio de tema
-  const selectedTheme = localStorage.getItem('selected-theme')
-  useEffect(() => {
-    checkTema()
-  }, [])
-  const getCurrentTheme = () => document.body.classList.contains('dark-theme') ? 'dark' : 'light'
-
-  const checkTema = () => {
-    const userHasDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-
-    if (selectedTheme) {
-      // Si se cumple la validación, preguntamos cuál fue el tema para saber si activamos o desactivamos el dark
-      document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove']('dark-theme')
-    } else {
-      // Preguntamos si el usuario tiene tema dark en su sistema
-      // En caso de que sí, lo activamos en la interfaz
-      if (userHasDarkTheme) document.body.classList.add('dark-theme')
-    }
-
-  }
-
-  const switchTema = () => {
-    document.body.classList.toggle('dark-theme')
-    // Guardamos el tema actual que eligió el usuario
-    localStorage.setItem('selected-theme', getCurrentTheme())
-  }
-
   const { user, editMode, changeEditMode, datosUser } = useAuth();
   const auth = getAuth();
+  const { mobile, switchTema } = useContext(UserConfig);
 
   const logout = () => signOut(auth);
   const handleLogout = async () => {
@@ -43,83 +18,154 @@ export function Nav() {
   }
 
   return (
-    <header className={style.contenedor}>
-      {user ? 
-        <div
-          class={style.usuario}>
-          <li>
-            {datosUser?.rol}:
-          </li>
-          <li>
-            <span>
-              {datosUser?.nombre}
-            </span>
-          </li>
-        </div>
-        :
-        null
-      }
-      {user == null ? <div
-        className={style.login}>
-        <ul
-          className={style.contenedorlista}>
-          <li>
-            <Link
-              className={style.loginlinks}
-              to={"/login"}>Login
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={style.loginlinks}
-              to={"/register"}>Registrarse
-            </Link>
-          </li>
-        </ul>
-      </div> :
-        <ul
-          className={style.contenedorlista}>
-          <li
-            className={style.elementolista}>
+    <>
+      {mobile ?
+        <header className={style.contenedorMobile}>
+          {user ?
+            <div
+              class={style.usuario}>
+              <li>
+                <span>
+                  {datosUser?.nombre}
+                </span>
+              </li>
+            </div>
+            :
+            null
+          }
+          {user == null ?
+            //para registrarse o loguearse
+            <div
+              className={style.login}>
+              <ul
+                className={style.contenedorlista}>
+                <li>
+                  <Link
+                    className={style.loginlinks}
+                    to={"/login"}>Login
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={style.loginlinks}
+                    to={"/register"}>Registrarse
+                  </Link>
+                </li>
+              </ul>
+            </div> :
+            //barra para navegador ya logueado
+            <ul
+              className={style.contenedorlista}>
+              <li
+                className={style.elementolista}>
 
-            <Link
-              className={style.links}
-              to={"/"}>
-              Home
-            </Link>
-          </li>
-          <li
-            className={style.elementolista}>
-            <Link
-              className={style.links}
-              to={"/cursos"}>
-              Cursos
-            </Link>
-          </li>
-          <li
-            className={style.elementolista}
-          >
-            <button
-              className={editMode ? style.botonActivo : style.links}
-              onClick={() => changeEditMode(datosUser?.rol)}
-            >
-              Editar:{editMode ? " on" : " off"}
-            </button>
-          </li>
-          <li
-            className={style.elementolista}>
-            <button
-              className="switchTema"
-              onClick={() => switchTema()}>
-            </button>
-          </li>
-          <li
-            className={style.elementolista}>
-            <button
-              className={style.links}
-              onClick={handleLogout}>Logout</button>
-          </li>
-        </ul>}
-    </header>
+                <Link
+                  className={style.links}
+                  to={"/"}>
+                  Home
+                </Link>
+              </li>
+              <li
+                className={style.elementolista}>
+                <Link
+                  className={style.links}
+                  to={"/cursos"}>
+                  Cursos
+                </Link>
+              </li>
+              <li
+                className={style.elementolista}>
+                <button
+                  className={style.links}
+                  onClick={handleLogout}>Logout</button>
+              </li>
+            </ul>
+          }
+        </header>
+        :
+        <header className={style.contenedor}>
+          {user ?
+            <div
+              class={style.usuario}>
+              <li>
+                {datosUser?.rol}:
+              </li>
+              <li>
+                <span>
+                  {datosUser?.nombre}
+                </span>
+              </li>
+            </div>
+            :
+            null
+          }
+          {user == null ?
+            //para registrarse o loguearse
+            <div
+              className={style.login}>
+              <ul
+                className={style.contenedorlista}>
+                <li>
+                  <Link
+                    className={style.loginlinks}
+                    to={"/login"}>Login
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={style.loginlinks}
+                    to={"/register"}>Registrarse
+                  </Link>
+                </li>
+              </ul>
+            </div> :
+            //barra para navegador ya logueado
+            <ul
+              className={style.contenedorlista}>
+              <li
+                className={style.elementolista}>
+
+                <Link
+                  className={style.links}
+                  to={"/"}>
+                  Home
+                </Link>
+              </li>
+              <li
+                className={style.elementolista}>
+                <Link
+                  className={style.links}
+                  to={"/cursos"}>
+                  Cursos
+                </Link>
+              </li>
+              <li
+                className={style.elementolista}
+              >
+                <button
+                  className={editMode ? style.botonActivo : style.links}
+                  onClick={() => changeEditMode(datosUser?.rol)}
+                >
+                  Editar:{editMode ? " on" : " off"}
+                </button>
+              </li>
+              <li
+                className={style.elementolista}>
+                <button
+                  className="switchTema"
+                  onClick={() => switchTema()}>
+                </button>
+              </li>
+              <li
+                className={style.elementolista}>
+                <button
+                  className={style.links}
+                  onClick={handleLogout}>Logout</button>
+              </li>
+            </ul>
+          }
+        </header>
+      }
+    </>
   )
 }
