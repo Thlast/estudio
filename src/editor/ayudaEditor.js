1650e6090b31742833f8dbea00864427a1e31064
Thlast <santiagoturini@gmail.com>
2023-05-21 18:29:17 -0300

    ayudaEditor terminado

--- src/editor/ayudaEditor.js
+++ src/editor/ayudaEditor.js
@@ -1,11 +1,51 @@
 import { useState } from "react";
 import { Previsualizacion } from "./previsualizacion";
+import { ejemplosMarkdown } from "./ejemplosMarkdown";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import remarkGfm from "remark-gfm";
 
 
 export function AyudaEditor(props) {
 
   const { preview } = props;
+  const { curso } = props;
   const [previsualizar, setPrevisualizar] = useState(false);
+  const ejemplos = ejemplosMarkdown
+
+  const mostrarEjemplo = (num) => {
+    const elemento = document.getElementById(`ayuda${num}`);
+    for (let i = 0; i < ejemplos.length; i++) {
+      if(i !== num){
+        document.getElementById(`boton${i}`).classList.remove("botonmostrar")
+      }
+    }
+    if (!document.getElementById(`boton${num}`).classList.contains("botonmostrar")) {
+      document.getElementById(`boton${num}`).classList.add("botonmostrar");
+    } else {
+      document.getElementById(`boton${num}`).classList.remove("botonmostrar");
+    }
+    
+
+    if (window.getComputedStyle(elemento).getPropertyValue("display") == "none") {
+      for (let i = 0; i < ejemplos.length; i++) {
+        if (document.getElementById(`ayuda${i}`)) {
+          document.getElementById(`ayuda${i}`).style.display = "none"
+        }
+      }
+      elemento.style.display = "block"
+    } else if (window.getComputedStyle(elemento).getPropertyValue("display") == "block") {
+      elemento.style.display = "none"
+    }
+
+  }
+  const señalarResultado = (num, numE) => {
+    if (document.querySelector(".RemarcarAyudaEditor")) {
+      document.querySelector(".RemarcarAyudaEditor")?.classList.remove("RemarcarAyudaEditor")
+      document.querySelector(".RemarcarAyudaEditor")?.classList.remove("RemarcarAyudaEditor")
+    }
+    document.getElementById(`demostracion-${numE}${num}`).classList.add("RemarcarAyudaEditor")
+    document.getElementById(`input-${numE}${num}`).classList.add("RemarcarAyudaEditor")
+  }
 
   return (
     <div className="secciones">
@@ -13,18 +53,74 @@ export function AyudaEditor(props) {
         <button
           onClick={() => setPrevisualizar(true)}
           className={`${previsualizar && "botonmostrar"} cursos-as editarcurso`}>
-            Previsualizacion</button>
+          Previsualizacion</button>
         <button
           onClick={() => setPrevisualizar(false)}
           className={`${!previsualizar && "botonmostrar"} cursos-as editarcurso`}>
-            Ayuda</button>
+          Ayuda</button>
       </div>
       <hr></hr>
-      {previsualizar ?  
+      {previsualizar ?
         <Previsualizacion preview={preview} />
         :
         <>
-          <section>
+          {ejemplos?.map((ej, numE) => {
+            if (!ej.curso || ej.curso == curso)
+              return (
+                <div
+                  key={"ejemplo-" + numE}>
+                  <button
+                    id={"boton" + numE}
+                    style={{ width: "100%" }}
+                    onClick={() => mostrarEjemplo(numE)}
+                    className="home-boton">
+                    {ej.nombre}{ej.curso ? ` (solo para ${ej.curso})` : null}
+                  </button>
+                  <section
+                    id={"ayuda" + numE}
+                    style={{ display: "none" }}
+                  >
+                    <br></br>
+                    <p>
+                      {ej.explicacion}
+                    </p>
+                    <div className="ayudaInput">
+                      {ej.texto?.map((a, num) => {
+                        return (
+                          <div
+                            onMouseEnter={() => señalarResultado(num, numE)}
+                            id={"input-" + numE + num}
+                            key={"input-" + numE + num}>
+                            <p>
+                              {a}
+                            </p>
+                          </div>
+                        )
+                      })}
+                    </div>
+                    <hr></hr>
+                    {ej.texto?.map((a, num) => {
+                      return (
+                        <div
+                          onMouseEnter={() => señalarResultado(num, numE)}
+                          id={"demostracion-" + numE + num}
+                          key={"demostracion-" + numE + num}>
+                          <ReactMarkdown
+                            remarkPlugins={[remarkGfm]}
+                          >
+                            {a}
+                          </ReactMarkdown>
+
+                        </div>
+                      )
+                    })}
+
+                    <hr></hr>
+                  </section>
+                </div>
+              )
+          })}
+          {/* <section>
             Headers
             # H1
             ## H2
@@ -40,7 +136,7 @@ export function AyudaEditor(props) {
 
             Alt-H2
             ------
-          </section>
+          </section> */}
         </>
       }
     </div>
---
+++ src/editor/ejemplosMarkdown.js
@@ -0,0 +1,100 @@
+export const ejemplosMarkdown = [
+    {
+        nombre: "Artículos ganancias",
+        explicacion: "Se pueden citar artículos de la ley de la siguiente manera:",
+        texto: [
+            'Ley texto ordenado: `Artículo 2`',
+            'Decreto Reglamentario: `Artículo 2 DR`',
+            'Ley procedimiento tributario: `Artículo 2 LPT`'
+        ],
+        curso: "impuestos"
+    },
+    {
+        nombre: "headers",
+        explicacion: "",
+        texto: [
+            '# Titulo: h1',
+            '## Titulo: h2',
+            '### Titulo: h3',
+            '#### Titulo: h4',
+            '##### Titulo: h5'
+        ]
+    },
+    {
+        nombre: "links",
+        explicacion: "Diferentes formas de poner enlaces, las referencias en el ejemplo se añaden en la misma linea pero pueden y deberían agregarse abajo del todo del input para tener mayor claridad.",
+        texto: [
+            '[Soy un enlace](https://www.google.com)',
+            '[Soy un enlace con titulo](https://www.google.com "Googles Homepage")',
+            'Link en linea <http://www.example.com>',
+            '[Link con REFERENCIA texto][texto de REFERENCIA] \n\n [texto de REFERENCIA]: https://www.mozilla.org',
+            '[REFERENCIA numerica link][1] \n\n [1]: http://slashdot.org',
+            'O REFERENCIAs con corchete y texto [link text itself]. \n\n [link text itself]: http://www.reddit.com',
+            '[link relativo a la sección 2 del capitulo 1](../../1/Impuesto%20a%20las%20ganancias/2)',
+            '[link relativo a la sección 19 del capitulo 1 de auditoria](../../../auditoria/11/capitulo1/19)',
+            '[link relativo a para transformar en tablas](/texto-a-tabla)',
+            '[link relativo a la home](/)'
+        ]
+    },
+    {
+        nombre: "imagenes",
+        explicacion: "![texto que se ve en caso de no cargar la imagen](link de la imagen 'texto que se ve al dejar el mouse encima')",
+        texto: [
+            '![texto](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png)',
+            '![alt text](/favicon.ico "Logo Title Text 1")',
+            '![alt text][logo] \n\n luego lo referencias de esta forma: \n\n  [logo]: http://localhost:3000/favicon.ico "Logo Title Text 2"'
+        ]
+    },
+    {
+        nombre: "Consola",
+        explicacion: "De esta forma se puede pasar texto a la consola para ver más información",
+        texto: [
+            '`aspecto subjetivo`, `hecho imponible`, `precio de transferencia`',
+        ]
+    },
+    {
+        nombre: "Tablas",
+        explicacion: "En el siguiente enlace se puede transformar de texto a tabla",
+        texto: [
+            "[Transformar de texto a tabla en este enlace](/texto-a-tabla)"
+        ]
+    },
+    {
+        nombre: "Separadores",
+        explicacion: "Tres o más, hay tres opciones (*), (-) o (_):",
+        texto: [
+            "---",
+            "***",
+            "___"
+        ]
+    },
+    {
+        nombre: "Estilos",
+        explicacion: "",
+        texto: [
+            "párrafo normal",
+            "> Esto es una cita.",
+            "**bloque destacado**",
+            "remarcar *palabra* en un párrafo",
+            "~~Tachar palabras.~~"
+        ]
+    },
+    {
+        nombre: "Listas",
+        explicacion: "",
+        texto: [
+            "1- No funciona, pero sirve para numerar",
+            "1) 1.si no hay espacio de ve el 1.",
+            "1. Aca se identa y no se ve la numeración",
+            "- Igual al anterior",
+            "- - se puede identar mas de una vez"
+        ]
+    },
+    {
+        nombre: "Videos de Youtube",
+        explicacion: "Con el id del video reemplazar: [![Texto si no carga](http://img.youtube.com/vi/YOUTUBE_VIDEO_ID_ACA/0.jpg)](http://www.youtube.com/watch?v=YOUTUBE_VIDEO_ID_ACA)",
+        texto: [
+            "[![Video de](http://img.youtube.com/vi/b7aQqAFio2s/0.jpg)](http://www.youtube.com/watch?v=dQw4w9WgXcQ)"
+        ]
+    }
+] 
