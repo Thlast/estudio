59a7cc4e9774747206d7aa3b28104af210044f0c
Thlast <santiagoturini@gmail.com>
2023-05-30 12:37:24 -0300

    boton cerrar consola

--- src/paginasCaps/consola.js
+++ src/paginasCaps/consola.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import { MostrarDef } from '../components/definiciones/mostrarDef';
@@ -6,9 +6,11 @@ import { getSeccionConsola, obtenerDatosConsola } from '../components/servicios/
 import { getSeccionPorId } from '../components/servicios/cursos/obtenerSeccion';
 import { Articulos } from '../components/dataInformes/articulos';
 import { buscarConsolaSQL } from '../components/servicios/cursos/cursosSQL/buscarSeccion';
+import { UserConfig } from '../context/UserConfig';
 
 export function Consola(props) {
 
+  const { mobile } = useContext(UserConfig)
   const [datos, setDatos] = useState()
   const [datosSeccion, setDatosSeccion] = useState()
   const { curso } = props;
@@ -53,10 +55,10 @@ export function Consola(props) {
 
       //setCargando(false)
       return () => controller.abort()
-    } 
+    }
     //buscar articulos si el curso=impuestos
     else if (curso == "impuestos" && (dic.includes("artículo") || dic.includes("articulo"))) {
-      
+
       setArticulo(dic)
     }
   }, [dic])
@@ -67,10 +69,10 @@ export function Consola(props) {
       setArticulo()
       setDatosSeccion()
     }
-
+    setMostrarConsola(true)
   }, [dic, buscarSeccionId])
 
-//console.log(dic)
+  //console.log(dic)
   //cuando le paso directo el id del SVG
   useEffect(() => {
     if (buscarSeccionId) {
@@ -97,132 +99,141 @@ export function Consola(props) {
     document.getElementById("consol").scrollIntoView({ behavior: 'smooth' });
   }, [datos, dic])
 
+  const [mostrarConsola, setMostrarConsola] = useState(true)
+
   return (
-    <div>
-      <div
-        id="consol"
-        className="navconsola">
-        <p>
-          {/* {cargando ?
-            <div className='consolacargando'>
-              <Spinner></Spinner>
-            </div>
-            :
-            null
-          } */}
+    <>
+      {mobile ? null :
+        <div
+        style={{ textAlign: "right" }}
+        >
           <button
-            className="botonhistorial"
-            onClick={() => limpiarHistorial()}
-          >X
-          </button>Visto reciente:{" "}
-          {enconsola.map((a, num) => {
-            return (
-              <span
-                key={`historial-${a}`}>
-                <button
-                  className="botonhistorial"
-                  onClick={() => eliminarDelHistorial(a)}
-                >X
-                </button>
-                <code
-                  className="aa">
-                  {a}
-                </code>
-              </span>
-            )
-          })}
-        </p>
-        <p className="enconsola">
-          Consola:
-          {" " + dic}
-        </p>
-      </div>
+          className='boton home-boton'
+            onClick={() => setMostrarConsola(!mostrarConsola)}>
+            {mostrarConsola ? "Cerrar Consola" : "Abrir Consola"}
+          </button>
+        </div>
+      }
       <div
-        className="consola">
+        style={{ display: `${mostrarConsola ? "block" : "none"}` }}>
+        <div
+          id="consol"
+          className="navconsola">
+          <p>
+            <button
+              className="botonhistorial"
+              onClick={() => limpiarHistorial()}
+            >X
+            </button>Visto reciente:{" "}
+            {enconsola.map((a, num) => {
+              return (
+                <span
+                  key={`historial-${a}`}>
+                  <button
+                    className="botonhistorial"
+                    onClick={() => eliminarDelHistorial(a)}
+                  >X
+                  </button>
+                  <code
+                    className="aa">
+                    {a}
+                  </code>
+                </span>
+              )
+            })}
+          </p>
+          <p className="enconsola">
+            Consola:
+            {" " + dic}
+          </p>
+        </div>
+        <div
+          className="consola">
 
-        <>
-          {/* si existen definiciones se renderizan: */}
-          {articulo ?
-            <Articulos articulo={articulo} recargarFuncionClickcode={recargarFuncionClickcode} />
-            :
-            <>
-              <MostrarDef
-                recargarFuncionClickcode={recargarFuncionClickcode}
-                curso={curso}
-                dic={dic} />
-              <hr></hr>
-            </>
-          }
+          <>
+            {/* si existen definiciones se renderizan: */}
+            {articulo ?
+              <Articulos articulo={articulo} recargarFuncionClickcode={recargarFuncionClickcode} />
+              :
+              <>
+                <MostrarDef
+                  recargarFuncionClickcode={recargarFuncionClickcode}
+                  curso={curso}
+                  dic={dic} />
+                <hr></hr>
+              </>
+            }
 
-          {
-            datosSeccion ? <>
-              {datosSeccion?.map(s => {
-                return (
-                  <div
-                    key={"consola" + buscarSeccionId}
-                    class="show-element">
-                    <h1>{s.SeccionNombre}</h1>
-                    <ReactMarkdown
-                      remarkPlugins={[remarkGfm]}>
-                      {s.SeccionContenido}
-                    </ReactMarkdown>
+            {
+              datosSeccion ? <>
+                {datosSeccion?.map(s => {
+                  return (
+                    <div
+                      key={"consola" + buscarSeccionId}
+                      class="show-element">
+                      <h1>{s.SeccionNombre}</h1>
+                      <ReactMarkdown
+                        remarkPlugins={[remarkGfm]}>
+                        {s.SeccionContenido}
+                      </ReactMarkdown>
+                      <br></br>
+                      <blockquote>Link a la sección:
+                        <em
+                          style={{ textDecoration: "underline" }}>
+                          <a
+                            target="_blank"
+                            href={`${url}/cursosSQL/${curso}/${s.CapituloId}/${s.CapituloNombre}/${s.SeccionId}`}>
+                            {" "}{s.SeccionNombre}
+                          </a>
+                        </em>
+                      </blockquote>
+                      <hr></hr>
+
+                    </div>
+                  )
+                })}
+
+
+              </> : null
+            }
+            {/*^^si existen definiciones se renderizan:^^*/}
+            {cargando ? null :
+              datos?.[0] ?
+                datos[0].seccion.enunciado.length == 0 || dic === "" ? null :
+                  <>
+                    {datos[0].seccion.enunciado.map((b, num) => {
+                      return (
+                        <div
+                          key={"consola" + dic + num}
+                          class="show-element">
+                          <ReactMarkdown
+                            remarkPlugins={[remarkGfm]}>
+                            {b}
+                          </ReactMarkdown>
+                        </div>
+                      )
+                    })
+                    }
                     <br></br>
                     <blockquote>Link a la sección:
                       <em
                         style={{ textDecoration: "underline" }}>
                         <a
                           target="_blank"
-                          href={`${url}/cursosSQL/${curso}/${s.CapituloId}/${s.CapituloNombre}/${s.SeccionId}`}>
-                          {" "}{s.SeccionNombre}
+                          href={`${url}/cursos/${datos[0].curso}/${datos[0].capitulo}/${datos[0].seccion.nombre}`}>
+                          {" "}{datos[0].seccion.nombre}
                         </a>
                       </em>
                     </blockquote>
                     <hr></hr>
+                  </>
+                : null}
 
-                  </div>
-                )
-              })}
+          </>
 
-
-            </> : null
-          }
-          {/*^^si existen definiciones se renderizan:^^*/}
-          {cargando ? null :
-            datos?.[0] ?
-              datos[0].seccion.enunciado.length == 0 || dic === "" ? null :
-                <>
-                  {datos[0].seccion.enunciado.map((b, num) => {
-                    return (
-                      <div
-                        key={"consola" + dic + num}
-                        class="show-element">
-                        <ReactMarkdown
-                          remarkPlugins={[remarkGfm]}>
-                          {b}
-                        </ReactMarkdown>
-                      </div>
-                    )
-                  })
-                  }
-                  <br></br>
-                  <blockquote>Link a la sección:
-                    <em
-                      style={{ textDecoration: "underline" }}>
-                      <a
-                        target="_blank"
-                        href={`${url}/cursos/${datos[0].curso}/${datos[0].capitulo}/${datos[0].seccion.nombre}`}>
-                        {" "}{datos[0].seccion.nombre}
-                      </a>
-                    </em>
-                  </blockquote>
-                  <hr></hr>
-                </>
-              : null}
-
-        </>
+        </div>
 
       </div>
-
-    </div>
+    </>
   )
 }
