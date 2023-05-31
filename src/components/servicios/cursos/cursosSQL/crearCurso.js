33a9e28e35016465a95f4aad2697e69fd032557a
Thlast <santiagoturini@gmail.com>
2023-05-31 12:57:17 -0300

    manejo errores

--- src/components/servicios/cursos/cursosSQL/crearCurso.js
+++ src/components/servicios/cursos/cursosSQL/crearCurso.js
@@ -1,28 +1,36 @@
-import { alertasuccess } from "../../../alertas";
+import { alertafail, alertasuccess } from "../../../alertas";
 
 const urlserverSQL = process.env.REACT_APP_SERVERSQL_PRODUCTION_URL || process.env.REACT_APP_SERVERSQL_LOCAL_URL
-  
-export const crearCurso = async (CursoId, CursoNombre, CursoDescripcion) => {
 
+export const crearCurso = async (CursoId, CursoNombre, CursoDescripcion) => {
   const url = `${urlserverSQL}/crearCurso/${CursoId}`;
 
-  let respuesta = {};
-
-  await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify({
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
         CursoNombre: CursoNombre,
         CursoDescripcion: CursoDescripcion
-    }),
-    headers: {
-      'Content-Type': 'application/json'
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('Error en la solicitud: ' + response.status);
     }
-  }).then(res => res.json())
-    .catch(error => (console.error('Error:', error)))
-    .then(response => (
-      console.log('Curso agregado:', response),
-      alertasuccess("Curso agregado"),
-      respuesta = response)
-    )
-  return respuesta
-}
+
+    const data = await response.json();
+
+    console.log('Curso agregado:', data);
+    alertasuccess('Curso agregado');
+
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+    // Puedes lanzar una alerta de error aquí o realizar cualquier otra acción para manejar el error.
+    alertafail(`${error}`)
+    throw error; // Opcional: puedes relanzar el error para que se maneje más arriba en la cadena de llamadas.
+  }
+};
