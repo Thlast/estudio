9f94fa5e1c6ccfae785e32f0fca1c0b1c9a77eef
Thlast <santiagoturini@gmail.com>
2023-06-02 14:50:39 -0300

    rt en capitulo

--- src/paginasCaps/Secciones/textoCurso.js
+++ src/paginasCaps/Secciones/textoCurso.js
@@ -5,6 +5,7 @@ import { InformeAuditor } from '../../components/informeAuditor';
 import { obtenerDatosSeccion } from '../../components/servicios/cursos/obtenerSeccion';
 import { Spinner } from '../../components/Login/Spinner';
 import { useNavigate } from 'react-router-dom';
+import { Articulos } from '../../components/dataInformes/articulos';
 
 export function TextoCurso(props) {
 
@@ -13,6 +14,7 @@ export function TextoCurso(props) {
   const { titulo } = props;
   const [enunciado, setEnunciado] = useState()
   const [cargando, setCargando] = useState(false)
+  const [mostrarRT, setMostrarRT] = useState(false)
   const { recargarFuncionClickcode } = props
   const navigate = useNavigate()
 
@@ -31,65 +33,76 @@ export function TextoCurso(props) {
   }
 
   useEffect(() => {
-
     setEnunciado();
-    // Creamos el controlador para abortar la petición
-    const controller = new AbortController()
-    // Recuperamos la señal del controlador
-    const { signal } = controller
-    // Hacemos la petición a la API y le pasamos como options la señal
-    cargarPagina({ signal })
-    return () => controller.abort()
+
+    if (titulo == "Listado de Resoluciones técnicas") {
+      setMostrarRT(true)
+    }
+    else {
+      setMostrarRT(false)
+      // Creamos el controlador para abortar la petición
+      const controller = new AbortController()
+      // Recuperamos la señal del controlador
+      const { signal } = controller
+      // Hacemos la petición a la API y le pasamos como options la señal
+      cargarPagina({ signal })
+      return () => controller.abort()
+
+    }
   }, [seccion])
 
-
+console.log(seccion)
   return (
     <>
-      {cargando ? <Spinner></Spinner> :
-        <div>
-          <h1>
-            {seccion}
-          </h1>
-          {
+      {
+        mostrarRT ? <Articulos articulo={seccion} recargarFuncionClickcode={recargarFuncionClickcode} />
+          :
+
+          cargando ? <Spinner></Spinner> :
+            <div>
+              <h1>
+                {seccion}
+              </h1>
+              {/* {
             seccion == "Informes de auditor" ? <InformeAuditor recargarFuncionClickcode={recargarFuncionClickcode} /> : null
-          }
+          } */}
 
-          <br></br>
-          <div>
-            {enunciado?.length == 0 ? null :
-              enunciado?.map(a => {
-                return (
-                  <>
-                    {a.enunciado?.map((e, num) => {
+              <br></br>
+              <div>
+                {enunciado?.length == 0 ? null :
+                  enunciado?.map(a => {
+                    return (
+                      <>
+                        {a.enunciado?.map((e, num) => {
 
-                      return (
-                        <>
-                          <div
-                            className='show-element'
-                            key={seccion + num}>
-                            <ReactMarkdown
-                              remarkPlugins={[remarkGfm]}>
-                              {e}
-                            </ReactMarkdown>
-                          </div>
-                        </>
-                      )
-                    }
+                          return (
+                            <>
+                              <div
+                                className='show-element'
+                                key={seccion + num}>
+                                <ReactMarkdown
+                                  remarkPlugins={[remarkGfm]}>
+                                  {e}
+                                </ReactMarkdown>
+                              </div>
+                            </>
+                          )
+                        }
+                        )
+                        }
+                        {
+                          <blockquote>
+                            id: {a.SeccionId}
+                          </blockquote>
+                        }
+                      </>
                     )
-                    }
-                    {
-                      <blockquote>
-                        id: {a.SeccionId}
-                      </blockquote>
-                    }
-                  </>
-                )
-              })
+                  })
 
 
-            }
-          </div>
-        </div>
+                }
+              </div>
+            </div>
       }
     </>
   )
