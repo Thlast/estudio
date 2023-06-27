5145210580a3bc6d29eeb9f2fb66a3e44de8fb4f
Thlast <santiagoturini@gmail.com>
2023-06-27 14:33:13 -0300

    manejo de errores planetscale

--- src/components/servicios/cursos/cursosSQL/modifSeccion.js
+++ src/components/servicios/cursos/cursosSQL/modifSeccion.js
@@ -3,30 +3,34 @@ import { alertasuccess } from "../../../alertas";
 const urlserverSQL = process.env.REACT_APP_SERVERSQL_PRODUCTION_URL || process.env.REACT_APP_SERVERSQL_LOCAL_URL
   
 export const modificarSeccion = async (nombreSeccion, contenido, seccionId, event) => {
-
   const url = `${urlserverSQL}/modificarSeccion/${seccionId}`;
   event.preventDefault();
 
-  let respuesta = {};
+  try {
+    const response = await fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify({
+        SeccionNombre: nombreSeccion,
+        SeccionContenido: contenido
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
 
-  await fetch(url, {
-    method: 'PUT',
-    body: JSON.stringify({
-      SeccionNombre: nombreSeccion,
-      SeccionContenido: contenido
-    }),
-    headers: {
-      'Content-Type': 'application/json'
+    if (!response.ok) {
+      throw new Error('Error en la solicitud de modificación');
     }
-  }).then(res => res.json())
-    .catch(error => (console.error('Error:', error)))
-    .then(response => (
-      console.log('Seccion modificada:', response),
-      alertasuccess("Seccion modificada"),
-      respuesta = response)
-    )
-  return respuesta
-}
+
+    const data = await response.json();
+    console.log('Seccion modificada:', data);
+    alertasuccess("Seccion modificada");
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
 //para el editor rapido
 export const modificarSeccionNombre = async (nombreSeccion, seccionId, event) => {
 
