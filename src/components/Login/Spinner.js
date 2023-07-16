a76db282448b77233559a1e14000711f11c7ae78
Thlast <santiagoturini@gmail.com>
2023-07-16 01:02:51 -0300

    corregido el tamaño del svg

--- src/components/Login/Spinner.js
+++ src/components/Login/Spinner.js
@@ -10,7 +10,7 @@ export const Spinner = () => {
     //   <div></div>
     // </div>
     <div className={style.contenedorLoading}>
-      <svg className={style.svgprueba} viewBox="0 0 480 480">
+      <svg className={style.svgprueba} viewBox="0 0 80 100">
         <defs>
           <path id="line" d="M-5 75 Q5 50 30 50 Q60 50 60 30 Q60 10 40 10 Q20 10 20 40 L20 60 Q20 90 40 90 Q60 90 70 70" />
         </defs>
